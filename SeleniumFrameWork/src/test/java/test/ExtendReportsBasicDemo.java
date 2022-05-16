e9beb6d50720e8e07226ddacef7b485ec25c19f5
naveenmj7760 <naveenmj7760@LAPTOP-HBROTHS7>
2022-05-16 15:37:28 +0530

    Test1 only

--- SeleniumFrameWork/src/test/java/test/ExtendReportsBasicDemo.java
+++ SeleniumFrameWork/src/test/java/test/ExtendReportsBasicDemo.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtendReportsBasicDemo {
@@ -25,11 +26,24 @@ public class ExtendReportsBasicDemo {
         String projectPath = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
+		test1.log(Status.INFO, "starting Test Case");
 		driver.get("https://google.com");
-		driver.findElement(By.name("q")).sendKeys("Automation");
+		test1.pass("Navigated to google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("Automationaerhgjjkkk");
+		test1.pass("Enterd text in Searchbox");
+		
 		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		//driver.findElement(By.name("btnK")).click();
+		test1.pass("Pressed keyboard enter key");
+		
 		driver.close();
-		driver.quit();
+		//driver.quit();
+		test1.pass("Closed the browser");
+		
+		test1.info("Test Completed");
         
+		// calling flush writes everything to the log file
+        extent.flush();
 	}
 }
