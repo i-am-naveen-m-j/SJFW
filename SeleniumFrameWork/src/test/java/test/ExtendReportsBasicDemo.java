5d9aee143a162d005584ab1dfbf1b0b24898554a
naveenmj7760 <naveenmj7760@LAPTOP-HBROTHS7>
2022-05-16 15:47:11 +0530

    Google Seartch Test One Google Seartch Test Two

--- SeleniumFrameWork/src/test/java/test/ExtendReportsBasicDemo.java
+++ SeleniumFrameWork/src/test/java/test/ExtendReportsBasicDemo.java
@@ -33,15 +33,42 @@ public class ExtendReportsBasicDemo {
 		driver.findElement(By.name("q")).sendKeys("Automationaerhgjjkkk");
 		test1.pass("Enterd text in Searchbox");
 		
-		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		//driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
 		//driver.findElement(By.name("btnK")).click();
-		test1.pass("Pressed keyboard enter key");
+		//test1.pass("Pressed keyboard enter key");
 		
 		driver.close();
 		//driver.quit();
 		test1.pass("Closed the browser");
 		
 		test1.info("Test Completed");
+		
+		
+		//
+		
+		// creates a toggle for the given test, adds all log events under it    
+        ExtentTest test2 = extent.createTest("Google Seartch Test Two","this is a test to validate google search functionality");
+	
+        //String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		test2.log(Status.INFO, "starting Test Case");
+		driver.get("https://google.com");
+		test2.pass("Navigated to google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("Automationaerhgjjkkk");
+		test2.pass("Enterd text in Searchbox");
+		
+		//driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		//driver.findElement(By.name("btnK")).click();
+		//test2.fail("Pressed keyboard enter key");
+		
+		driver.close();
+		//driver.quit();
+		//test2.pass("Closed the browser");
+		test2.fail("Closed the browser");
+		
+		test2.info("Test Completed");
         
 		// calling flush writes everything to the log file
         extent.flush();
