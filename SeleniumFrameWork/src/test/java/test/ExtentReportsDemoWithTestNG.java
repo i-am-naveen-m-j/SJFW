b72d578a169073a72d5f6e3dad569f185178fe7a
naveenmj7760 <naveenmj7760@LAPTOP-HBROTHS7>
2022-05-16 18:39:23 +0530

    Selenium Framework for Beginners 11 _ How to use Extent Reports with TestNG in Selenium Java@4:53 video

---
+++ SeleniumFrameWork/src/test/java/test/ExtentReportsDemoWithTestNG.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ExtentReportsDemoWithTestNG {
+	
+	@BeforeSuite
+	public void setup() {
+	}
+	
+	
+	@Test
+	public void test1() {
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+	}
+}
